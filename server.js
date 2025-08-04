730a323902e3a9749152429d0c1887dd47b76658
dollemi3 <133764814+dollemi3@users.noreply.github.com>
2025-08-04 17:39:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/snippet", codeSnippetRoutes);
 app.use("/api/calendar", calendarRoutes);
 app.use("/api/task", taskRoutes);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
